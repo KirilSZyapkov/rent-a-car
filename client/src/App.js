e528b6dfc7a642685d4c5c85db52d3696a49ea20
Kiril Zyapkov <ziapkov@gmail.com>
2021-09-08 22:27:43 +0300

    correcting background image

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import './Components/container.css';
 
 function App() {
   return (
     <div className="App">
+      <div className = "container">
+        <h1>hi</h1>
+      </div>
       
     </div>
   );
