a4bc58de739e7dd2052abcaa66caa7e207d9afe8
Kiril Zyapkov <ziapkov@gmail.com>
2021-12-08 23:16:23 +0200

    Fixing small bugs and changes

--- server/config/databse.js
+++ server/config/databse.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
-const init = require('../init/init');
+const { DB } = require('../init/init');
 
 module.exports = (app) => {
     return new Promise((resolve, reject) => {
-        mongoose.connect(init.DB, {
+        mongoose.connect(DB, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
         const db = mongoose.connection;
-        db.on('error', (err)=>{
+        db.on('error', (err) => {
             console.log('Connection error!');
             reject();
         });
-        db.on('open',()=>{
+        db.on('open', () => {
             console.log('Connected to DB');
             resolve();
         })
--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const databaseConfig = require('./config/databse');
-const init = require('./init/init');
 const expressConfig = require('./config/expressConfig');
+const { HOST } = require('./init/init');
+
 
 start();
 async function start() {
     await databaseConfig(app);
     expressConfig(app);
     
-   
+   app.get('/', (req, res)=>{
+       res.send('Server is online').end();
+   })
 
-    app.listen(init.HOST, () => console.log(`Server is listening on port ${init.HOST}`));
+    app.listen(HOST, () => console.log(`Server is listening on port ${HOST}`));
 }
